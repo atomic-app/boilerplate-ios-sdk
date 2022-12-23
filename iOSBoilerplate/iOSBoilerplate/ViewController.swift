ac91ea148c278db7e7b2cfa046e1feb4ff6d5eac
Eric Yuan <eric@atomic.io>
2022-12-23 17:29:08 +1300

    Create empty project

---
+++ iOSBoilerplate/iOSBoilerplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBoilerplate
+//
+//  Created by Eric on 23/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
